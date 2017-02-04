df53fe598d3146c55dd413138005155e3bab2e53
skylifewww <skylife@ukr.net>
2017-02-04 16:41:01 +0200

    upda

--- settings/mail.py
+++ settings/mail.py
@@ -3,10 +3,11 @@ ADMINS = (
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'robot@pangolinfog.com'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.pangolin.com'
-EMAIL_HOST_USER = 'alexey@pangolin.com'
-EMAIL_HOST_PASSWORD = ''
+# DEFAULT_FROM_EMAIL = 'robot@pangolinfog.com'
+DEFAULT_FROM_EMAIL = 'skylifewww@gmail.com'
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'skylifewww@gmail.com'
+EMAIL_HOST_PASSWORD = 'skywww123'
 EMAIL_PORT = 587
